cc53a9ed0ac27e61f552b9ac1c608ceaa98d88f1
Tohirjon Yusupov <yusupovtohir12@gmail.com>
2025-05-14 15:52:15 +0500

    create get venue owner function

---
+++ backend/controllers/admin/getVenueOwner.js
@@ -0,0 +1,26 @@
+const pool = require("../../config/db");
+
+exports.getVenueOwner = async (req, res) => {
+  try {
+
+
+    
+
+    // Fetch the venue owner from the database
+    const venueOwner = await pool.query(
+      `select u.firstname, u.lastname, v.name from venues v
+inner join users u on v.owner_id = u.id `
+    );
+
+    // Check if the venue owner exists
+    if (venueOwner.rows.length === 0) {
+      return res.status(404).json({ error: "To'yxona egalari topilmadi" });
+    }
+
+    // Return the venue owner details
+    return res.status(200).json(venueOwner.rows);
+  } catch (error) {
+    console.error('Error fetching venue owner:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+}
