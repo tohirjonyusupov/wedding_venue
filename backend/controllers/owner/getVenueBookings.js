71e8c651b5cfea8d68042b361c8efc781fe1a478
Tohirjon Yusupov <yusupovtohir12@gmail.com>
2025-05-15 15:32:09 +0500

    create get venue booking function

---
+++ backend/controllers/owner/getVenueBookings.js
@@ -0,0 +1,21 @@
+const pool = require("../../config/db");
+
+exports.getVenueBookings = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const result = await pool.query(`
+      SELECT b.id, b.reservation_date, b.guest_count, b.status,
+             u.firstname, u.lastname, u.phone_number, v.name as venue_name
+      FROM bookings b
+      JOIN users u ON b.user_id = u.id
+      JOIN venues v ON b.venue_id = v.id
+      WHERE v.user_id = $1
+      ORDER BY b.reservation_date ASC;
+    `, [userId]);
+
+    res.status(200).json({ success: true, data: result.rows });
+  } catch (error) {
+    res.status(500).json({ message: 'Server xatosi', error: error.message });
+  }
+};
