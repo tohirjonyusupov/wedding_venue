ef7b6bc8a790d927528a829f0f18687c7e08b8a0
Tohirjon Yusupov <yusupovtohir12@gmail.com>
2025-05-15 15:39:01 +0500

    update client routes

--- backend/routes/clientRoutes.js
+++ backend/routes/clientRoutes.js
@@ -3,7 +3,15 @@ const express = require('express');
 const clientRoutes = express.Router();
 
 const { clientSignUp } = require('../controllers/client/ClientSignUp');
+const { getVenues } = require('../controllers/client/getVenues');
+const { createBooking } = require('../controllers/client/createBooking');
+const { getMyBookings } = require('../controllers/client/getMyBookings');
+const { cancelMyBooking } = require('../controllers/client/cancelMyBookings');
 
 clientRoutes.post('/signup', clientSignUp);
+clientRoutes.get('/venues', getVenues);
+clientRoutes.post('/bookings', createBooking);
+clientRoutes.get('/my-bookings', getMyBookings);
+clientRoutes.patch('/my-bookings/:id/cancel', cancelMyBooking);
 
 module.exports = clientRoutes;
