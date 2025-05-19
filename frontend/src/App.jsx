a89375299aa46e8f9d63e38d0b103d0fd3873095
Tohirjon Yusupov <yusupovtohir12@gmail.com>
2025-05-19 12:12:51 +0500

    update app jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
       </Route>
 
       <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
+        <Route path="/signup" element={<SignUp />} />
       </Route>
 
 
